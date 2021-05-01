35543888657b9fd4cdb334e47b6ef35e1192a731
IKoshelev <koshelew@live.com>
2021-05-01 17:34:43 +0200

    phase 2

--- src/multimethod.ts
+++ src/multimethod.ts
@@ -14,25 +14,24 @@ export function multimethod
 
 type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
 
-type BaseItemTagless = Expand<
-    Omit<TBaseType, TTagKey>
-    & { [key in TTagKey]: string }
->;
+type BaseItemTagless = Expand<Omit<TBaseType, TTagKey>>;
 
-type DerivedItem<TDerivedTag extends `${TBaseTypeTag}${string}`> = BaseItemTagless & {
-    [key in TTagKey]: TDerivedTag
+type PropertyDerivedTag = {
+    [key in TTagKey]: `${TBaseTypeTag}${string}`
 }
 
-type TMethodForInvocation = (item: DerivedItem<`${TBaseTypeTag}${string}`>, ...args: TArgs) => TReturn;
+type DerivedItem = BaseItemTagless & PropertyDerivedTag;
 
-type TOverrideForInvocation =  <
-    TDerivedType extends DerivedItem<`${TBaseTypeTag}${string}`>>
+type TMethodForInvocation = (item: Expand<DerivedItem>, ...args: TArgs) => TReturn;
+
+type TMethodForExtend =  <
+    TDerivedType extends DerivedItem>
 (tag: TDerivedType[TTagKey], method:  (item: TDerivedType, ...args: TArgs) => TReturn) => void;
 
 return {} as 
     TMethodForInvocation & {
         concreteMethods: Map<string, TMethodForInvocation>,
-        extend: TOverrideForInvocation,
+        extend: TMethodForExtend,
         //override: <TDerivedType extends TBaseType, TDerivedTag extends `${TBaseTypeTag}${string}`>(tag: TDerivedTag, method:  (item: TDerivedType, ...args: TArgs) => TReturn) => void,
     }
 };
